200d05ba651c79112f8b2f7e431c3d9f9a7956e4
Dexter Inguito <37497130+ddextroo@users.noreply.github.com>
2023-07-02 15:06:17 +0800

    Create smashwords.py

---
+++ smashwords.py
@@ -0,0 +1,9 @@
+def smash(words):
+    result = ''
+    for i in range(len(words)):
+        if i == len(words)-1:
+            result += words[i]
+        else:
+            result += words[i] + " "
+    return result
+    
